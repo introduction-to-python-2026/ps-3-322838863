c3a73b974fa29f764fef071a33e772191aa55ff0
322838863 <shovalgos@gmail.com>
2025-11-06 13:45:15 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,5 +1,5 @@
 import random
-num_points = 10000
+num_points = 100000
 in_or_out = []
 for i in range(num_points):
   in_or_out.append((((random.random()-0.5)**2) + (random.random()-0.5)**2)**0.5 <= 0.5)
