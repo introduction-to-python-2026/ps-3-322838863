cf6cac92987de6403ec9d6370f611b9ba266271c
322838863 <shovalgos@gmail.com>
2025-11-06 13:44:13 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,2 +1,7 @@
-def approximate_pi(n_terms):
-    pass # replace pass with your code
+import random
+num_points = 10000
+in_or_out = []
+for i in range(num_points):
+  in_or_out.append((((random.random()-0.5)**2) + (random.random()-0.5)**2)**0.5 <= 0.5)
+  tozaa = sum(in_or_out)/num_points*4
+print(tozaa)
