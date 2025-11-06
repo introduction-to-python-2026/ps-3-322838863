a1d5997a16970deb6a002eb0cb2cdd1a54f6b9c1
322838863 <shovalgos@gmail.com>
2025-11-06 14:00:50 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,6 +1,8 @@
 def approximate_pi(n_terms):
-  pi_approx = 0
+  pi_cal = 0
   for k in range(n_terms):
-    pi_approx +=(-1)**k / (2*k+1)
-  return 4*pi_approx
+    pi_cal +=(-1)**k / (2*k+1)
+  return 4*pi_cal
+print (approximate_pi(10))
+print (approximate_pi(100))
 print (approximate_pi(1000))
