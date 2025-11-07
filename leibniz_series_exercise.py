054bc4bac62ee85986e8e156dd65f44804de6f95
322838863 <shovalgos@gmail.com>
2025-11-07 22:34:55 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,6 +1,7 @@
-def approximate_pi(n_terms):
-  pi_estimate = 0
+def approximate_pi(n_terms) -> float:
+  pi_estimate = 0.0
   for k in range(n_terms):
-    pi_estimate = ((-1) ** k) / (2 * k + 1)
+    term = ((-1) ** k) / (2 * k + 1)
+    pi_estimate += term
   return 4 * pi_estimate
    
